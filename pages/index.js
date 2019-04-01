36086298f3f5ebd8d9db0190449b7a197a28b3f7
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-04-01 12:37:21 -0700

    Hello world, meet Shadow

--- pages/index.js
+++ pages/index.js
@@ -4,43 +4,9 @@ import Link from 'next/link'
 
 const Index = () => (
   <div>
-    <nav className="navbar">
-      <div className="container">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://live.hacksc.com">
-            <img src={logo} width="112"/>
-          </a>
-        </div>
-
-        <div className="navbar-menu">
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <a className="is-primary">
-                <strong>Live</strong>
-              </a>
-            </div>
-
-            <div className="navbar-item">
-              <a>
-                API Directory
-              </a>
-            </div>
-
-            <div className="navbar-item">
-              <a>
-                Resources
-              </a>
-            </div>
-
-            <div className="navbar-item">
-              <a>
-                FAQ
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <div className="container">
+      <h1 className="title">Shadow</h1>
+    </div>
   </div>
 )
 
